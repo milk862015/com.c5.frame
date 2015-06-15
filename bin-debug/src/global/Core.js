431077b233db486a3c25e4c7ede6418312957a94
milk86 <milk86@yeah.net>
2015-06-15 17:00:21 +0800

    删除啊bin-debug目录

--- bin-debug/src/global/Core.js
+++
@@ -1,8 +0,0 @@
-var Core = (function () {
-    function Core() {
-    }
-    var __egretProto__ = Core.prototype;
-    return Core;
-})();
-Core.prototype.__class__ = "Core";
-//# sourceMappingURL=Core.js.map
